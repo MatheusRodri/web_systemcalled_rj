5603adff40cbd8576560c90029fa6b7328477dce
Matheus <matheus.rj25@hotmail.com>
2023-01-04 17:02:24 -0300

    create page SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../../assets/logo.png'
+
+
 export default function SignUp(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
     return(
-        <h1>SignUp</h1>
+        <div className="container-center">
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='logo of system'/>
+                </div>
+
+                <form>
+                    <h1>Create your account</h1>
+                    <input type="text" placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)}/>
+                    <input type="text" placeholder="E-mail" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                    <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                    <button type='submit'>Create</button>
+                </form>
+
+                <Link to='/register'>Already an account ? Sign In</Link>
+
+            </div>
+        </div>
     );  
 }
