fd10e46add213636d13d5c0e213393602f071c15
Matheus <matheus.rj25@hotmail.com>
2023-01-04 16:41:08 -0300

    create page SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,29 @@
+import './signin.css'
+import logo from '../../assets/logo.png'
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
+
 export default function SignIn(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
     return(
-        <h1>SignIn</h1>
+        <div className="container-center">
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='logo of system'/>
+                </div>
+
+                <form>
+                    <h1>SignIn</h1>
+                    <input type="text" placeholder="E-mail" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                    <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                    <button type='submit'>Access</button>
+                </form>
+
+                <Link to='/register'>Create a new account</Link>
+
+            </div>
+        </div>
     );  
 }
