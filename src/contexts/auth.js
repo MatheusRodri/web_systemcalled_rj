cbf2187e0fe7862e177f7ac8349978af5f133540
Matheus <matheus.rj25@hotmail.com>
2023-01-05 20:15:09 -0300

    add logout and private route

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,8 +1,9 @@
-import { useState, createContext, useEffect } from "react";
+import { useState, createContext,useEffect} from "react";
 import { auth, db } from "../services/firebaseConnection";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
@@ -13,7 +14,23 @@ export const AuthContext = createContext({});
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
+
   const navigate = useNavigate();
+
+  useEffect(()=>{
+    async function loadUser(){
+        const storageUser = localStorage.getItem("@userLocal");
+
+        if(storageUser){
+            setUser(JSON.parse(storageUser));
+            setLoading(false);
+        }
+        setLoading(false);
+    }
+    loadUser();
+  },[])
+
 
   async function signIn(email, password) {
     setLoadingAuth(true);
@@ -81,6 +98,12 @@ export default function AuthProvider({ children }) {
     localStorage.setItem("@userLocal", JSON.stringify(data));
   }
 
+  async function logout(){
+    await signOut(auth);
+    localStorage.removeItem('@userLocal');
+    setUser(null);
+  }
+
   return (
     <AuthContext.Provider
       value={{
@@ -88,7 +111,9 @@ export default function AuthProvider({ children }) {
         user,
         signIn,
         signUp,
+        logout,
         loadingAuth,
+        loading,
       }}
     >
       {children}
